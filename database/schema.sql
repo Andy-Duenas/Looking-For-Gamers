8748d6fe53fb70064ad8aac07bb77c68a0a0be7a
Andy Duenas <Duenas.g.andy@gmail.com>
2021-04-06 15:17:17 -0700

    working on table

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,28 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+create table "userInfo" (
+  "userId"      serial,
+  "username"    text not null unique,
+  "password"    text not null,
+  "createdAt"   timestamptz(6) not null default now(),
+  primary key ("userId")
+);
+
+create table "gameInfo" (
+  "gameId"       serial,
+  "title"        text not null unique,
+  "img"          text not null,
+  "description"  text not null,
+  primary key ("gameId")
+);
+
+create table "postsInfo" (
+  "gameId"      serial,
+  "userId"      text not null,
+  "post"        text not null,
+  "createdAt"   timestamptz(6) not null default now(),
+  FOREIGN KEY (gameId) REFERENCES gameInfo(gameId),
+  FOREIGN KEY (userId) REFERENCES userInfo(userId)
+);
