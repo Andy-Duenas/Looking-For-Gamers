728b9f944a6794c533adb4fece525adea2f5d02b
Andy Duenas <Duenas.g.andy@gmail.com>
2021-04-12 15:10:36 -0700

    removed useless code and checked to make sure everythings working.#3

--- client/pages/search.jsx
+++ client/pages/search.jsx
@@ -6,8 +6,7 @@ export default class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchFor: [],
-      singleGame: {}
+      searchFor: []
     };
     this.makeGameList = this.makeGameList.bind(this);
   }
